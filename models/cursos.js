51213d28e7a46903b75375f9ba4b1c16f9804317
ciscoreno12 <ciscomalakian00@gmail.com>
2024-05-25 04:38:20 -0300

    Estructura inicial

---
+++ models/cursos.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cursos extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Cursos.init({
+    comision: DataTypes.STRING,
+    turno: DataTypes.STRING,
+    fechaInicio: DataTypes.DATE,
+    fechaFin: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Cursos',
+  });
+  return Cursos;
+};
