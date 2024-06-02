72cb5d9034725e61e8c349c548f95737291006d2
CasasolaFranco <franco.casasola@estudiantes.unahur.edu.ar>
2024-06-02 17:55:33 -0300

    Se corrigio el POST /carreras

--- src/routes/carreraRoute.js
+++ src/routes/carreraRoute.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const db = require('../db/models')
 const carreraController = require('../controllers/carreraController')
 const middlewareCarrera = require('../middlewares/existsMiddleware')
+const carreraSchema = require('../schemas/carreraSchema')
 const route = Router()
 
 route.get('/carreras', carreraController.getAllCarreras)
@@ -10,7 +11,7 @@ route.get('/carreras/:id', middlewareCarrera.existsById(db.Carrera), carreraCont
 //route.post('/carreras/:id/materia', ...) 201,404,400
 //route.get('/carreras/:id/materia', ...) 200,404
 
-route.post('/carreras', middlewareCarrera.validaSchema ,carreraController.crearCarrera)
+route.post('/carreras', middlewareCarrera.validaSchema(carreraSchema) ,carreraController.crearCarrera)
 
 
 //  creo que estos no hacen falta
