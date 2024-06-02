8c1c38668b039c4bacd2a4e7edecc2acf3bf6bdf
CasasolaFranco <franco.casasola@estudiantes.unahur.edu.ar>
2024-06-02 14:02:20 -0300

    Se crearon instancias de cada modelo para testear las tablas y sus relaciones

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cursoRoute = require('./routes/cursoRoute')
 const profesorRoute = require('./routes/profesorRoute')
 const materiaRoute = require('./routes/materiaRoute')
 const cursoProfesorRoute = require('./routes/cursoProfesorRoute')
+const db = require('./db/models')
 
 const app = express()
 app.use(express.json())
@@ -17,5 +18,40 @@ app.use(cursoProfesorRoute)
 
 app.listen(3000, async ()=>{
     console.log(`La aplicacion arranco correctamente en el puerto 3000`);
+
+    try {
+        await db.sequelize.authenticate();
+        await db.sequelize.sync({force:true});
+
+        const carreraTest = db.Carrera.create({
+            nombre: 'carreraTest',
+            grado: '1',
+            universidad: 'UTN'
+        });
+
+        const materiaTest = db.Materia.create({
+            nombre: 'materiaTest',
+            cuatrimestral: 1,
+            anio: 1,
+            carreraId: 1
+        });
+
+        const cursoTest = db.Cursos.create({
+            comision: 'A',
+            turno: 'tarde',
+            fechaInicio: new Date('2024-06-02'),
+            fechaFin: new Date('2024-06-10'),
+            materiaId: 1
+        });
+
+        const profesorTest = db.Profesor.create({
+            nombre: 'profesorTest',
+            fechaNacimiento: new Date('2001-01-28'),
+            legajo: 1,
+            activo: 1
+        })
+    } catch (error) {
+        console.log(error);
+    }
 })
 
