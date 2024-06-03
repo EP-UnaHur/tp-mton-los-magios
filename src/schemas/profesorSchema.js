a607a001973d27a29e9f55b9002de1ecc04f0bcf
CasasolaFranco <franco.casasola@estudiantes.unahur.edu.ar>
2024-06-03 12:17:32 -0300

    se agrego un schema solo para id's de profesores y una forma de validar su legajo

--- src/schemas/profesorSchema.js
+++ src/schemas/profesorSchema.js
@@ -1,5 +1,6 @@
 const Joi = required('Joi')
 const validateDate = require('../utils/dateValidator')
+const validateLegajo = require('../utils/validateLegajo')
 
 const profesorSchema = joy.object().keys({
     nombre: Joi.string().required().min(1).max(20).messages({
@@ -9,20 +10,27 @@ const profesorSchema = joy.object().keys({
         'any.required': 'El nombre es obligatorio'
     }),
 
-    fechaNacimiento: Joi.string().custom(validateDate).required.messages({
+    fechaNacimiento: Joi.string().custom(validateDate).required().messages({
         "any.custom": "El formato de la fecha debe ser YYYY-MM-DD",
         'string.empty': 'La fecha de nacimiento no puede estar vacía',
         'any.required': 'La fecha de nacimiento es obligatoria'
     }),
 
-    legajo: Joi.number().optional().messages({
+    legajo: Joi.number().optional().custom(validateLegajo).messages({
         'number.base': 'El campo legajo debe ser un número entero',
     }),
 
-    activo: Joi.boolean().optional().messages({
+    activo: Joi.boolean().messages({
         'boolean.base': 'El campo activo debe ser un booleano'
     })
 
+})
+
+const profesorSoloIdSchema = joy.object.keys({
+    id: Joi.number().required().messages({
+        'number.base': 'El campo id debe ser un numero entero',
+        'any.required': 'El id es obligatorio'
+    })
 })
 
 module.exports = profesorSchema
